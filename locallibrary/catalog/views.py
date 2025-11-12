ca75858ec22bd95198318a258d8843148865321e
Korkin-Igor <korkinigorttit@gmail.com>
2025-11-12 11:41:47 +0700

    add search book by find word in title

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -3,33 +3,32 @@ from .models import Book, Author, BookInstance, Genre
 from django.views import generic
 
 def index(request):
-    """
-    Функция отображения для домашней страницы сайта.
-    """
-    # Генерация "количеств" некоторых главных объектов
+    search_word = request.GET.get('search_word', '')  # ← получаем ДО context
+
+
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-    # Доступные книги (статус = 'a')
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_authors = Author.objects.count()
     num_genres = Genre.objects.count()
-    search_word = 'book'
     num_books_with_word = Book.objects.filter(title__icontains=search_word).count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_with_word': num_books_with_word,
+        'search_word': search_word,  # ← теперь переменная точно определена
+    }
 
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
         request,
         'index.html',
-        context={
-            'num_books':num_books,
-            'num_instances':num_instances,
-            'num_instances_available':num_instances_available,
-            'num_authors':num_authors,
-            'num_genres':num_genres,
-            'num_books_with_word': num_books_with_word,
-            'search_word': search_word,
-        },
+        context=context,
     )
 
 class BookListView(generic.ListView):
