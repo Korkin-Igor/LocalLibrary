14c4684ec7cd78f5fd7e4bab5bed31a61e064d7c
Korkin-Igor <korkinigorttit@gmail.com>
2025-11-12 09:45:33 +0700

    set up templates, view, url for main page (index)

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -19,10 +19,13 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
     path('catalog/', include('catalog.urls')),
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# from django.conf import settings
+# from django.conf.urls.static import static
+#
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
