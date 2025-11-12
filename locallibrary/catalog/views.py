1938e9acad46d5c07094abbbf09c095b31d04ca8
Korkin-Igor <korkinigorttit@gmail.com>
2025-11-12 11:29:20 +0700

    add search word and count genres in home page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -7,18 +7,29 @@ def index(request):
     Функция отображения для домашней страницы сайта.
     """
     # Генерация "количеств" некоторых главных объектов
-    num_books=Book.objects.all().count()
-    num_instances=BookInstance.objects.all().count()
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
     # Доступные книги (статус = 'a')
-    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
-    num_authors=Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_genres = Genre.objects.count()
+    search_word = 'book'
+    num_books_with_word = Book.objects.filter(title__icontains=search_word).count()
 
     # Отрисовка HTML-шаблона index.html с данными внутри
     # переменной контекста context
     return render(
         request,
         'index.html',
-        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
+        context={
+            'num_books':num_books,
+            'num_instances':num_instances,
+            'num_instances_available':num_instances_available,
+            'num_authors':num_authors,
+            'num_genres':num_genres,
+            'num_books_with_word': num_books_with_word,
+            'search_word': search_word,
+        },
     )
 
 class BookListView(generic.ListView):
