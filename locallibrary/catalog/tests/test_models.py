9060ee611856273bfc8fe039549c2e1162b99c30
Korkin-Igor <korkinigorttit@gmail.com>
2025-11-13 11:45:21 +0700

    fix test

--- locallibrary/catalog/tests/test_models.py
+++ locallibrary/catalog/tests/test_models.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from catalog.models import Author
 
-
 class AuthorModelTest(TestCase):
 
     @classmethod
@@ -41,11 +40,11 @@ class AuthorModelTest(TestCase):
 
     def test_object_name_is_last_name_comma_first_name(self):
         author = Author.objects.get(id=1)
-        expected_object_name = '{0}, {1}'.format(author.last_name, author.first_name)
+        expected_object_name = f"{author.last_name} {author.first_name}"
 
         self.assertEqual(str(author), expected_object_name)
 
     def test_get_absolute_url(self):
         author = Author.objects.get(id=1)
         # This will also fail if the urlconf is not defined.
-        self.assertEqual(author.get_absolute_url(), '/catalog/author/1')z
+        self.assertEqual(author.get_absolute_url(), '/catalog/author/1')
